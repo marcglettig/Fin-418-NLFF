860cfc6ad3e4eb9ebcb5582a58933304fe268710
marcglettig <marc.glettig@epfl.ch>
2020-12-27 15:25:26 +0100

    Initial working version of webscraper

--- webscraper.py
+++ webscraper.py
@@ -1,32 +1,78 @@
 import argparse
 import requests
-
+import re
+import os
 from bs4 import BeautifulSoup
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Scrape Yahoo Finance for articles related to a Stock symbol')
-    parser.add_argument('symbol', metavar='S', type=str, help='The stock symbol to be scraped for')
+from article import Article
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Scrape on PR Newswire for articles related to a Stock')
+    parser.add_argument('symbol', metavar='S', type=str, help='The stock symbol or Company name to be scraped for')
 
     args = parser.parse_args()
     print("running webscrape for " + args.symbol)
 
-    URL = "https://finance.yahoo.com/quote/" + args.symbol + "?p=" + args.symbol
-    press_URL = "https://finance.yahoo.com/quote/" + args.symbol + "/press-releases?p=" + args.symbol
+    URL = "https://www.prnewswire.com/search/news/?keyword=" + args.symbol
+    print("Checking on: " + URL)
+    page_number = 0
 
-    page = requests.get(URL)
-    press_page = requests.get(press_URL)
-    print("Checking on: " + press_URL)
+    # Creating folder structure and setting output path
+    module_path = os.path.dirname(os.path.realpath(__file__))
+    output_dir = os.path.join(module_path, 'data/' + args.symbol)
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
 
-    soup = BeautifulSoup(page.content, 'html.parser')
-    press_soup = BeautifulSoup(page.content, 'html.parser')
+    article_number = 0
 
-    press_results = press_soup.find_all(id='Main')
-    for result in press_results:
-        press_elements = result.find_all('li', class_='js-stream-content Pos(r)')
-        elems = 0
-        for elem in press_elements:
-            elems += 1
-            title_elem = elem.find('h3', class_='Mb(5px)')
+    # Loop over articles until no more found.
+    while True:
+        page_number += 1
+        try:
+            page = requests.get(URL + '&pagesize=25&page=' + str(page_number))
+        except ValueError:
+            print('URL not found')
+            break
 
-            print(title_elem)
-        print(elems)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        press_results = soup.find(class_='container search-results-text')
+        press_results = press_results.find_all('a', 'news-release', 'href')
+        print("result on page: " + str(len(press_results)))
+        if len(press_results) == 0:
+            break
+
+        # Checking every single article in search results
+        for result in press_results:
+            print("Checking: " + result['href'])
+
+            # Initializing article information values
+            URL_ = "https://www.prnewswire.com" + result['href']
+            page_ = requests.get(URL_)
+            soup_ = BeautifulSoup(page_.content, 'html.parser')
+            info = soup_.find(class_='container release-header')
+            title = info.find('h1')
+            subtitle = info.find('p', 'subtitle')
+            date = info.find('p', 'mb-no')
+
+            # Stripping additional content from webpage and only keeping text paragraphs
+            body = soup_.find(class_='release-body container')
+            paragraphs = body.find_all('p')
+
+            # Cumulatively building article content string
+            clean_body = ''
+            for para in paragraphs:
+                if para.string:
+                    clean_body += para.string
+
+            art = Article(title, subtitle, date, clean_body)
+
+            # Saving the article to the specified output path
+            output_path = os.path.join(output_dir, 'article_' + str(article_number) + '.txt')
+            article_number += 1
+            with open(output_path, 'w+') as article_file:
+                article_file.write(str(art))
+
+
+if __name__ == '__main__':
+    main()
